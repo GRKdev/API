3859c5cab129d9f433b2db779b7cb1a982366f7a
RK <gorkvilar@gmail.com>
2023-09-02 20:38:15 +0200

    añadido rutas facturacion

--- functions/albaranes_f_stat.py
+++ functions/albaranes_f_stat.py
@@ -7,7 +7,6 @@ def obtener_importe_por_mes(nombre_cliente):  # http://localhost:5000/api/alb_st
     search_terms = nombre_cliente.split(',')
     search_string = " ".join(['"' + term + '"' for term in search_terms])
 
-    # Obtener el nombre real del cliente desde la base de datos
     nombre_real_cliente = collection.find_one({"$text": {"$search": search_string}}, {"NombreCliente": 1})
     if nombre_real_cliente:
         nombre_real_cliente = nombre_real_cliente.get("NombreCliente", "")
@@ -40,7 +39,88 @@ def obtener_importe_por_mes(nombre_cliente):  # http://localhost:5000/api/alb_st
         output_dict[meses[month - 1]] = f"{importe} €"
 
     return {
-        "NombreCliente": nombre_real_cliente,  # Usamos el nombre real del cliente
+        "NombreCliente": nombre_real_cliente, 
         "Año": current_year,
         "IngresosMensuales": output_dict
+    }
+
+def obtener_totales_anuales(_=None): #http://localhost:5000/api/alb_stat?total=true
+    collection = db['CabeceraAlbaran']
+    pipeline = [
+        {
+            "$group": {
+                "_id": {"year": {"$year": "$FechaAlbaran"}},
+                "ImporteTotal": {"$sum": "$ImporteFactura"}
+            }
+        },
+        {"$sort": {"_id.year": 1}}
+    ]
+    results = collection.aggregate(pipeline)
+    output_list = []
+
+    for r in results:
+        year = r["_id"]["year"]
+        importe = r["ImporteTotal"]
+        output_list.append({"Año": year, "Cantidad": f"{importe} €"})
+    return {
+        "IngresosAnuales": output_list
+    }
+
+def obtener_meses_currentyear(value=None):    #http://localhost:5000/api/alb_stat?t_m_cy=true
+    current_year = datetime.now().year 
+    collection = db['CabeceraAlbaran']
+    pipeline = [
+        {"$match": {"FechaAlbaran": {"$gte": datetime(current_year, 1, 1), "$lt": datetime(current_year + 1, 1, 1)}}},
+        {
+            "$group": {
+                "_id": {"month": {"$month": "$FechaAlbaran"}},
+                "ImporteTotal": {"$sum": "$ImporteFactura"}
+            }
+        },
+        {"$sort": {"_id.month": 1}}
+    ]
+    results = collection.aggregate(pipeline)
+    meses = [
+        'Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun',
+        'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic'
+    ]
+    output_dict = {mes: "0 €" for mes in meses}
+    for r in results:
+        month = r["_id"]["month"]
+        importe = r["ImporteTotal"]
+        output_dict[meses[month - 1]] = f"{importe} €"
+
+    return {
+        "IngresosMensuales": output_dict,
+        "Año": current_year
+    }
+
+def obtener_meses_selectedyear(year):  # http://localhost:5000/api/alb_stat?t_m_y=2022
+    selected_year = int(year)
+    collection = db['CabeceraAlbaran']
+    pipeline = [
+        {"$match": {"FechaAlbaran": {"$gte": datetime(selected_year, 1, 1), "$lt": datetime(selected_year + 1, 1, 1)}}},
+        {
+            "$group": {
+                "_id": {"month": {"$month": "$FechaAlbaran"}},
+                "ImporteTotal": {"$sum": "$ImporteFactura"}
+            }
+        },
+        {"$sort": {"_id.month": 1}}
+    ]
+    results = collection.aggregate(pipeline)
+    meses = [
+        'Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun',
+        'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic'
+    ]
+    output_dict = {mes: "0 €" for mes in meses}
+    for r in results:
+        month = r["_id"]["month"]
+        importe = r["ImporteTotal"]
+        output_dict[meses[month - 1]] = f"{importe} €"
+
+    return {
+        "IngresosMensuales": output_dict,
+        "Año": selected_year
+
     }
--- routes/albaranes/albaranes_stat.py
+++ routes/albaranes/albaranes_stat.py
@@ -1,11 +1,14 @@
 from flask import request, jsonify
 from extensions import app
-from functions.albaranes_f_stat import obtener_importe_por_mes
+from functions.albaranes_f_stat import obtener_importe_por_mes, obtener_totales_anuales, obtener_meses_currentyear, obtener_meses_selectedyear
 
 @app.route('/api/alb_stat', methods=['GET'])
 def get_albaran_stat():
     params_mapping = {
         'cli_mes': obtener_importe_por_mes,
+        'total': obtener_totales_anuales,
+        't_m_cy': obtener_meses_currentyear,
+        't_m_y': obtener_meses_selectedyear,
     }
     combined_results = []
     for param, function in params_mapping.items():
@@ -15,8 +18,8 @@ def get_albaran_stat():
             if isinstance(results, dict):
                 results = [results]
             combined_results.extend(results)
-    
+
     if not combined_results:
         return jsonify({'error': 'Parámetro no reconocido o faltante'}), 400
-    
+
     return jsonify(combined_results)
